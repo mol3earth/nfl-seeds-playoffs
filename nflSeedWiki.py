d4880c9d3ed66443e4a93da8fa7d9b967ffff497
mol3earth <mol3earth@gmail.com>
2020-01-21 15:17:03 -0700

    added main(), updated scraper per wikipedia change

--- nflSeedWiki.py
+++ nflSeedWiki.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jan  2 18:41:26 2020
-
 @author: mol3earth
 """
 
@@ -11,7 +10,131 @@ from bs4 import BeautifulSoup as bs4
 
 debugMode=True
 baseUrl = 'https://en.m.wikipedia.org/'
-playOffStr = {}
+
+class playOffSeedsAndStrengthOfSchedule:
+    '''
+        play off seeds and strength of schedule
+        stores the structure created and the print summary functions
+    '''
+    
+    def __init__(self):
+        self.playOffStr = {}    
+    
+    def printSoSbySeed(self):
+        '''
+        Will print the mean Strength of Schedule for each Seed
+        '''
+        print('-'*50)
+        print('Strength of Schedule by Seed')
+        print('-'*50)
+        pp='\t'
+        print('Seed  ' + 'MaxSoS '+' MinSoS '+' AvgSoS')
+        SoSbS=[]
+        for n in range(0,7):
+            SoSbS.append([]);  
+        for k in self.playOffStr.keys():
+            for seed in range(1,7):
+                SoSbS[seed].append(int(self.playOffStr[k]['Seeding']['AFC'][seed]['SoS']['Rank']))
+                SoSbS[seed].append(int(self.playOffStr[k]['Seeding']['NFC'][seed]['SoS']['Rank']))
+        for seed in range(1,7):
+            print(str(seed)+'  '+pp+'\t'.join([
+                                 str(min(SoSbS[seed])),
+                                 str(max(SoSbS[seed])),
+                                 str(sum(SoSbS[seed])/len(SoSbS[seed]))[0:4]
+                  ]))
+        
+    def printSoSByYear(self):
+        '''
+        Will print the Strength of Schedules of each team by year.
+        '''
+        print('-'*50)
+        print('Strength of Schedule by Year')
+        print('-'*50)
+        pp='\t'
+        tArr={}
+        tArr['max']=[]
+        tArr['min']=[]
+        tArr['avg']=[]
+        print(' Year         ' + 'MaxSoS '+' MinSoS '+' AvgSoS')
+        for k in self.playOffStr.keys():
+            tArr[k]=[]
+            for team in self.playOffStr[k]['POSoS'].keys():
+                tArr[k].append( int(self.playOffStr[k]['POSoS'][team]) )
+            tArr['max'].append(max(tArr[k]))
+            tArr['min'].append(min(tArr[k]))
+            tArr['avg'].append(sum(tArr[k])/len(tArr[k]))
+            print(k+'  '+pp+'\t'.join( [str(min(tArr[k])),
+                                 str(max(tArr[k])),
+                                 str(sum(tArr[k])/len(tArr[k]))[0:4]
+                 ]))
+        print('Averages'+pp+'\t'.join( [str(sum(tArr['min'])/len(tArr['min']))[0:4],
+                             str(sum(tArr['max'])/len(tArr['max']))[0:4],
+                             str(sum(tArr['avg'])/len(tArr['avg']))[0:4]
+              ]))
+    
+    def printSoSWinLossAndDiffByRoundAndSeed(self):
+        '''
+        Will print the Wins, Losses, and SoS Differential for each round in the playoffs. 
+        '''
+        print('-'*50)
+        print('Average Strength of Schedule of Winners (AWSoS)\n' +
+              'Average Strength of Schedule of Losers (ALSoS)\n' +
+              'Average Strength of Schedule of Winners (ADSoS)\n' +
+              'by Round and by Seed')
+        print('-'*50)
+        wbSoS={};    lbSoS={};    dbSoS={}    
+        wbrSoS={};    lbrSoS={};    dbrSoS={} 
+        for rnd in ['WC','DV','CC','SB']:
+            wbrSoS[rnd]=[] ; lbrSoS[rnd]=[] ; dbrSoS[rnd]=[] ; 
+        wbsSoS=[];    lbsSoS=[];    dbsSoS=[];
+        pp='\t'
+        for n in range(0,7):
+            wbsSoS.append([]);    lbsSoS.append([]);    dbsSoS.append([]);
+        for k in self.playOffStr.keys():
+            wbSoS[k]=[]
+            lbSoS[k]=[]
+            dbSoS[k]=[]
+            for rnd in self.playOffStr[k]['roundStr']:
+                for game in self.playOffStr[k]['roundStr'][rnd]:
+                    wSoS = int(game['Winner']['SoS'])
+                    lSoS = int(game['Loser']['SoS'])
+                    dSoS = lSoS - wSoS
+                    wbSoS[k].append(wSoS)
+                    lbSoS[k].append(lSoS)
+                    dbSoS[k].append(dSoS)
+                    wbrSoS[rnd].append(wSoS)
+                    lbrSoS[rnd].append(lSoS)
+                    dbrSoS[rnd].append(dSoS)
+                    wbsSoS[game['Winner']['Seed']].append(wSoS)
+                    lbsSoS[game['Winner']['Seed']].append(lSoS)
+                    dbsSoS[game['Winner']['Seed']].append(dSoS) 
+                    
+            #print(k+'  '+pp+'\t'.join( [
+            #             str(sum(wbSoS[k])/len(wbSoS[k]))[0:4],
+            #             str(sum(lbSoS[k])/len(lbSoS[k]))[0:4],
+            #             str(sum(dbSoS[k])/len(dbSoS[k]))[0:4],
+            #             ]))
+        headers = pp +'\t'.join(['AWSoS','ALSoS','ADSoS'])
+        print('Rnd' + headers)
+        for rnd in wbrSoS.keys():
+            print(rnd+'  '+pp+'\t'.join( [
+                 str(sum(wbrSoS[rnd])/len(wbrSoS[rnd]))[0:4],
+                 str(sum(lbrSoS[rnd])/len(lbrSoS[rnd]))[0:4],
+                 str(sum(dbrSoS[rnd])/len(dbrSoS[rnd]))[0:4],
+                 ]))
+        print('')
+        print('Seed' + headers)
+        for seed in range(1,7):
+            print(str(seed)+'  '+pp+'\t'.join( [
+                 str(sum(wbsSoS[seed])/len(wbsSoS[seed]))[0:4],
+                 str(sum(lbsSoS[seed])/len(lbsSoS[seed]))[0:4],
+                 str(sum(dbsSoS[seed])/len(dbsSoS[seed]))[0:4],
+                 ]))
+        
+    def printAll(self):
+        self.printSoSbySeed()
+        self.printSoSByYear()
+        self.printSoSWinLossAndDiffByRoundAndSeed()
 
 def getSoS(table):
     '''
@@ -64,7 +187,13 @@ def getTeamInfo(wikiUrl):
     debugMode=False
     if debugMode:
         print('\n   '+ ' scraping: ' + wikiUrl)
-    infoTableRows = bs4( get( baseUrl + wikiUrl ).text ).find('table' , {'class': 'infobox vcard'}).find_all('tr')
+    infoTableRows = bs4( 
+                        get( baseUrl + wikiUrl ).text,
+                        features="html5lib" 
+                        ).find(
+                                'table' , 
+                                {'class': 'infobox vcard'} 
+                               ).find_all('tr')
     noID = True
     resultID = 0
     while noID:
@@ -198,7 +327,7 @@ def popStuff(Winner, Loser, winsBySeed, winsBySoS, rnd, roundsBySeed, roundStr, 
                             'Loser': Loser , 
                             'Date' : DateTime ,
                             'Score' : Score
-                            })
+                        })
     return winsBySoS, winsBySeed, roundsBySeed, roundStr
 
 def Hparse(h4, DateTime, seeds, SoS):
@@ -243,8 +372,9 @@ def getRoundsByManyDivs(divs, seeds, SoS):
     '''
     winsBySoS, winsBySeed, roundsBySeed, roundStr = initStuff()
     for divID in [3, 4, 5, 6]:
-        print(divID)
-        print(divs[divID].find_all('h2'))
+        if debugMode:
+            print(divID)
+            print(divs[divID].find_all('h4'))
         for h4 in divs[divID].find_all('h4'):
             Winner, Loser , Score, DateTime = Hparse(h4 ,  h4.findPrevious('h3').text.strip('Edit'), seeds, SoS)
             if divID == 3:
@@ -262,131 +392,48 @@ def getRoundsByManyDivs(divs, seeds, SoS):
             print('-*'*50 + '\n       DONE WITH  ' + rnd)
     return winsBySoS, winsBySeed, roundsBySeed, roundStr
 
-def printSoSByYear(playOffStr):
+def main(yearStart=2003,yearEnd=2018):
     '''
-    Will print the Strength of Schedules of each team by year.
+    Takes a range of years and returns a structure of Playoff performance by 
+        seed and strenght of schedule. 
+        input: 
+                yearStart: integer, default is 2003 (playoff restructured to 
+                                                     current)
+                yearEnd: integer, range end inclusive, defaults to 2018.
     '''
-    tArr={}
-    tArr['max']=[]
-    tArr['min']=[]
-    tArr['avg']=[]
-    pp='\t'
-    print(pp+'     ' + 'Max SoS '+'Min SoS '+'Avg SoS')
-    for k in playOffStr.keys():
-        tArr[k]=[]
-        for team in playOffStr[k]['POSoS'].keys():
-            tArr[k].append( int(playOffStr[k]['POSoS'][team]) )
-        tArr['max'].append(max(tArr[k]))
-        tArr['min'].append(min(tArr[k]))
-        tArr['avg'].append(sum(tArr[k])/len(tArr[k]))
-        print(k+'  '+pp+'\t'.join( [str(min(tArr[k])),
-                             str(max(tArr[k])),
-                             str(sum(tArr[k])/len(tArr[k]))[0:4]
-                             ]))
-    print('Averages'+pp+'\t'.join( [str(sum(tArr['min'])/len(tArr['min']))[0:4],
-                         str(sum(tArr['max'])/len(tArr['max']))[0:4],
-                         str(sum(tArr['avg'])/len(tArr['avg']))[0:4]
-                         ]))
-
-def printWinLossAndDiffBySoSandRound(playOffStr):
-    '''
-    Will print the Wins, Losses, and SoS Differential for each round in the playoffs. 
-    '''
-    pp='\t'
-    print(pp+'     ' + 'Max SoS '+'Min SoS '+'Avg SoS')
-    wbSoS={};    lbSoS={};    dbSoS={}    
-    wbrSoS={};    lbrSoS={};    dbrSoS={} 
-    for rnd in ['WC','DV','CC','SB']:
-        wbrSoS[rnd]=[] ; lbrSoS[rnd]=[] ; dbrSoS[rnd]=[] ; 
-    wbsSoS=[];    lbsSoS=[];    dbsSoS=[];
-    for n in range(0,7):
-        wbsSoS.append([]);    lbsSoS.append([]);    dbsSoS.append([]);
-    for k in playOffStr.keys():
-        wbSoS[k]=[]
-        lbSoS[k]=[]
-        dbSoS[k]=[]
-        for rnd in playOffStr[k]['roundStr']:
-            for game in playOffStr[k]['roundStr'][rnd]:
-                wSoS = int(game['Winner']['SoS'])
-                lSoS = int(game['Loser']['SoS'])
-                dSoS = lSoS - wSoS
-                wbSoS[k].append(wSoS)
-                lbSoS[k].append(lSoS)
-                dbSoS[k].append(dSoS)
-                wbrSoS[rnd].append(wSoS)
-                lbrSoS[rnd].append(lSoS)
-                dbrSoS[rnd].append(dSoS)
-                wbsSoS[game['Winner']['Seed']].append(wSoS)
-                lbsSoS[game['Winner']['Seed']].append(lSoS)
-                dbsSoS[game['Winner']['Seed']].append(dSoS) 
-                
-        print(k+'  '+pp+'\t'.join( [
-                     str(sum(wbSoS[k])/len(wbSoS[k]))[0:4],
-                     str(sum(lbSoS[k])/len(lbSoS[k]))[0:4],
-                     str(sum(dbSoS[k])/len(dbSoS[k]))[0:4],
-                     ]))
-    for rnd in wbrSoS.keys():
-        print(rnd+'  '+pp+'\t'.join( [
-             str(sum(wbrSoS[rnd])/len(wbrSoS[rnd]))[0:4],
-             str(sum(lbrSoS[rnd])/len(lbrSoS[rnd]))[0:4],
-             str(sum(dbrSoS[rnd])/len(dbrSoS[rnd]))[0:4],
-             ]))
-    for seed in range(1,7):
-        print(str(seed)+'  '+pp+'\t'.join( [
-             str(sum(wbsSoS[seed])/len(wbsSoS[seed]))[0:4],
-             str(sum(lbsSoS[seed])/len(lbsSoS[seed]))[0:4],
-             str(sum(dbsSoS[seed])/len(dbsSoS[seed]))[0:4],
-             ]))
-    ######
-
-def printSoSbySeed(playOffStr):
-    '''
-    Will print the mean Strength of Schedule for each Seed
-    '''
-    pp='\t'
-    print('     ' + 'Max SoS '+'Min SoS '+'Avg SoS')
-    SoSbS=[]
-    for n in range(0,7):
-        SoSbS.append([]);  
-    for k in playOffStr.keys():
-        for seed in range(1,7):
-            SoSbS[seed].append(int(playOffStr[k]['Seeding']['AFC'][seed]['SoS']['Rank']))
-            SoSbS[seed].append(int(playOffStr[k]['Seeding']['NFC'][seed]['SoS']['Rank']))
-    for seed in range(1,7):
-        print(str(seed)+'  '+pp+'\t'.join([
-                             str(min(SoSbS[seed])),
-                             str(max(SoSbS[seed])),
-                             str(sum(SoSbS[seed])/len(SoSbS[seed]))[0:4]
-                             ]))
-    
-
-for year in range(2003,2019):
-    if year < 2003:
-        print('*'*50 + '\n  CAN NOT GET Strength of Schedule FOR YEARS < 2003 ')
-        print('      UNLESS YOU FIND A BETTER SITE THAN teamrankings.com ')
-        quit
-    print('DOING '+str(year))
-    yrRng = str(year)+'-'+str(year+1)[-2:]
-    playOffStr[yrRng] = {}
-    # get SoS 
-    SoStable = bs4(get('https://www.teamrankings.com/nfl/ranking/schedule-strength-by-other?date=' +
-        str(year+1) + 
-        '-02-28').text).find('table')
-    playOffStr[yrRng]['SoS'] = getSoS(SoStable)
-    # get Wiki tables
-    pg = bs4( get(   baseUrl + 
-                 "/wiki/" + # + "Template:"
-                 str(year) + 
-                 "%E2%80%93" + 
-                 str(year+1)[-2:] + 
-                 "_NFL_playoffs"
-            ).text )
-    tables = pg.find_all('table', {'class':'wikitable'})                                    # 2
-    playOffStr[yrRng]['Seeding'] = getSeeding(tables[0])
-    playOffStr[yrRng]['Seeding'], playOffStr[yrRng]['POSoS'] = findSoS(playOffStr[yrRng]['SoS'], playOffStr[yrRng]['Seeding'])
-    if year > 2005:                                                                                                                                  ## 4  
-        playOffStr[yrRng]['WBSoS'], playOffStr[yrRng]['WBS'], playOffStr[yrRng]['RBS'], playOffStr[yrRng]['roundStr'] = getRoundsByScheduleTable(tables[1], playOffStr[yrRng]['Seeding']['Seeds'] ,  playOffStr[yrRng]['POSoS'])
-    else:
-        divs = pg.find_all('div', {'class': 'collapsible-block'})
-        playOffStr[yrRng]['WBSoS'], playOffStr[yrRng]['WBS'], playOffStr[yrRng]['RBS'], playOffStr[yrRng]['roundStr'] = getRoundsByManyDivs( divs, playOffStr[yrRng]['Seeding']['Seeds'],  playOffStr[yrRng]['POSoS'])
-      
+    playOffStr={}
+    posasos = playOffSeedsAndStrengthOfSchedule()
+    for year in range(yearStart,yearEnd+1):
+        if year < 2003:
+            print('*'*50 + '\n  CAN NOT GET Strength of Schedule FOR YEARS < 2003 ')
+            print('      UNLESS YOU FIND A BETTER SITE THAN teamrankings.com ')
+            quit
+        if debugMode:
+            print('DOING '+str(year))
+        yrRng = str(year)+'-'+str(year+1)[-2:]
+        playOffStr[yrRng] = {}
+        # get SoS 
+        sosGet = get('https://www.teamrankings.com/nfl/ranking/schedule-strength-by-other?date=' +
+                     str(year+1) + 
+                     '-02-28').text
+        SoStable = bs4( sosGet , features="html5lib" ).find('table')
+        playOffStr[yrRng]['SoS'] = getSoS(SoStable)
+        # get Wiki tables
+        wikiGet = get(    baseUrl + 
+                          "/wiki/" + # + "Template:"
+                          str(year) + 
+                          "%E2%80%93" + 
+                          str(year+1)[-2:] + 
+                          "_NFL_playoffs"
+                     ).text
+        pg = bs4(  wikiGet , features="html5lib" )
+        tables = pg.find_all('table', {'class':'wikitable'})                                    # 2
+        playOffStr[yrRng]['Seeding'] = getSeeding(tables[0])
+        playOffStr[yrRng]['Seeding'], playOffStr[yrRng]['POSoS'] = findSoS(playOffStr[yrRng]['SoS'], playOffStr[yrRng]['Seeding'])
+        if year > 2005:                                                                                                                                  ## 4  
+            playOffStr[yrRng]['WBSoS'], playOffStr[yrRng]['WBS'], playOffStr[yrRng]['RBS'], playOffStr[yrRng]['roundStr'] = getRoundsByScheduleTable(tables[1], playOffStr[yrRng]['Seeding']['Seeds'] ,  playOffStr[yrRng]['POSoS'])
+        else:
+            divs = pg.find_all('section', {'class': 'collapsible-block'})
+            playOffStr[yrRng]['WBSoS'], playOffStr[yrRng]['WBS'], playOffStr[yrRng]['RBS'], playOffStr[yrRng]['roundStr'] = getRoundsByManyDivs( divs, playOffStr[yrRng]['Seeding']['Seeds'],  playOffStr[yrRng]['POSoS'])
+    posasos.playOffStr = playOffStr
+    return posasos
